566aa1e75b8dcfc490c1c4a811962be792de694a
Jeffrey Chen <jheff.chen@mail.utoronto.ca>
2021-11-15 15:06:10 -0500

    first function

---
+++ R/RscriptName.R
@@ -0,0 +1,30 @@
+#' PCA Plot for Set of Attributes in a population
+#'
+#' This function will display a PCA plot of n attributes to see how
+#' closely related certain species are related to each other
+#'
+#'
+#' @param attributeSet The set of attributes
+#' @return Returns a PCA plot of different species relatedness
+#'
+#'
+#' @examples
+#' pcaPopulationPlot(mtcars)
+#'
+#' @references
+#'
+#' @export
+#'
+#' @import ggbiplot
+
+pcaPopulationPlot <- function(attributeSet) {
+
+  #store attribute set into popPCA
+  popPCA <- prcomp(attributeSet, center = TRUE, scale = TRUE)
+
+  #plot popPCA into a plot
+  populationPlot <- ggbiplot(popPCA, labels=rownames(popPCA))
+  return(populationPlot)
+}
+
+# [END]
